1574a8d922d5751baeb56d6ba9f53fc73bf55c3b
Durga Prasad <durgadp444@gmail.com>
2025-08-12 01:46:58 +0530

    added few tests and page classes

---
+++ src/main/java/dp/pages/CartPage.java
@@ -0,0 +1,54 @@
+package dp.pages;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import dp.utils.WaitUtils;
+
+public class CartPage {
+	WebDriver driver;
+	Actions a;
+
+	public CartPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		a = new Actions(driver);
+	}
+
+	@FindBy(xpath = "//tbody//tr//td[@class='cart_description']//a")
+	List<WebElement> productsinCart;
+
+	@FindBy(xpath = "//a[contains(@class,'check_out')]")
+	private WebElement checkoutBtn;
+
+	@FindBy(xpath = "//u[text()='Register / Login']")
+	private WebElement registerOrLogin;
+
+	public WebElement getProductsInCart(String productName) {
+
+		WebElement cartProd = productsinCart.stream().filter(product -> product.getText().contains(productName))
+				.findFirst().orElse(null);
+		return cartProd;
+	}
+
+	public void goToCheckOut() {
+		WaitUtils.visibilityOfElementLocated(driver, checkoutBtn, 10).click();
+	}
+
+	public void goToRegisterOrLogin() {
+		WaitUtils.visibilityOfElementLocated(driver, registerOrLogin, 10).click();
+
+	}
+
+	public boolean askToRegisterOrLogin() {
+		WebElement registerOrlogin = WaitUtils.visibilityOfElementLocated(driver, registerOrLogin, 10);
+		return registerOrlogin.isDisplayed();
+	}
+
+}
--- src/test/java/dp/tests/Productscatalogue.java
+++ src/test/java/dp/tests/Productscatalogue.java
@@ -1,6 +1,6 @@
 package dp.tests;
 
-
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -8,79 +8,75 @@ import org.testng.annotations.Test;
 import dp.Base.BaseTest;
 import dp.pages.ProductPage;
 import dp.pages.RegisterPage;
+import dp.pages.CartPage;
+import dp.pages.CheckOutPage;
 
 public class Productscatalogue extends BaseTest {
-		
-	
-		String productName = "Men Tshirt";
-		
-	@Test(invocationCount=1)
-	public void ProductsList() throws InterruptedException {
-		
-		ProductPage productPage =new ProductPage(driver);
+
+	String productName = "Men Tshirt";
+
+	@Test(priority=1)
+	public void addProdToCart() throws InterruptedException {
+		// Verify the products added to cart shouldn't be able to checkout without
+		// login/registering
+		ProductPage productPage = new ProductPage(driver);
 		productPage.goToProducts();
 		productPage.selectMenTshirt();
-        Assert.assertTrue(productPage.validateMenTshirtHeader());
-        productPage.getProductByName(productName);
-        productPage.addProductToCart(productName);
-        Assert.assertEquals(productPage.getToastMes(), "Added!");
-        System.out.println(productPage.getToastMes());
-        productPage.goToCart();
-        WebElement cartProduct =productPage.getProductsInCart(productName);
-        Assert.assertTrue(cartProduct.getText().contains("Tshirt"),
-                "Cart does not contain the expected product: " + productName);
-        productPage.goToCheckOut();
-        Assert.assertTrue(productPage.askToRegisterOrLogin(),"Register/Login button should be displayed when checking out without logging in");
-        
-        
-        
+		Assert.assertTrue(productPage.validateMenTshirtHeader());
+		productPage.getProductByName(productName);
+		productPage.addProductToCart(productName);
+		Assert.assertEquals(productPage.getToastMes(), "Added!");
+		System.out.println(productPage.getToastMes());
+		productPage.goToViewCart();
+		productPage.goToCheckOut();
+		Assert.assertTrue(productPage.askToRegisterOrLogin(),
+				"Register/Login button should be displayed when checking out without logging in");
+	}
 
-        
-//		WaitUtils.waitForElementToBeClickable(driver, By.xpath("//a[@href='#Men']"), 10).click();
-////		driver.findElement(By.xpath("//a[@href='#Men']")).click();
-//		WaitUtils.waitForElementToBeClickable(driver, By.xpath("//*[text()='Tshirts ']"), 10).click();
-////		driver.findElement(By.xpath("//*[text()='Tshirts ']")).click();
-//		String tShirtHeader = driver.findElement(By.xpath("//*[text()='Men - Tshirts Products']")).getText();
-//		String expectedTshirtHeader = "Men - Tshirts Products";
-//		Assert.assertTrue(expectedTshirtHeader.equalsIgnoreCase(tShirtHeader));
-//		List<WebElement> Tshirts = driver
-//				.findElements(By.xpath("//div[@class='features_items']/div[@class='col-sm-4']"));
-//		WebElement prod = Tshirts.stream()
-//				.filter(product -> product.getText().contains(productName)).findFirst()
-//				.orElse(null);
-//		prod.findElement(By.cssSelector(".btn")).click();
-//		String toast = WaitUtils.visibilityOfElementLocated(driver, By.cssSelector(".modal-content div h4"), 10)
-//				.getText();		
-////		String toast = driver.findElement(By.cssSelector(".modal-content div h4")).getText();
-//		Assert.assertEquals(toast, "Added!");
+	@Test(priority=2)
+	public void verifyCartPage() {
+		// login and verify if the same product added is reflecting in cart page
+		// and able to checkout
+		String email = "MPRASAD@gmail.com";
+		String password = "Hello@123";
+		RegisterPage registerPage = new RegisterPage(driver);
+		registerPage.clickSignupLink();
+		registerPage.enterEmailAndPassword(email, password);
+		registerPage.clickLoginButton();
+		ProductPage productPage = new ProductPage(driver);
+		productPage.goToProducts();
+		productPage.getProductByName(productName);
+		productPage.addProductToCart(productName);
+		productPage.goToViewCart();
+		CartPage cartPage = new CartPage(driver);
+		WebElement cartProduct = cartPage.getProductsInCart(productName);
+		Assert.assertTrue(cartProduct.getText().contains(productName));
 
 	}
-	
-		@Test(enabled=true)
-		public void verifyCartPage() {
-			
-			String email = "MPRASAD@gmail.com";
-			String password = "Hello@123";
-			RegisterPage registerPage = new RegisterPage(driver);
-			registerPage.clickSignupLink();
-			registerPage.enterEmailAndPassword(email, password);
-			registerPage.clickLoginButton();
-			ProductPage productPage =new ProductPage(driver);
-			productPage.goToProducts();
-	        productPage.getProductByName(productName);
-	        productPage.addProductToCart(productName);
-	        productPage.goToCart();
-	        
-	    
 
+	@Test(dependsOnMethods= {"verifyCartPage"})
+	public void checkOutCartAndPayment() throws InterruptedException {
+		String email = "MPRASAD@gmail.com";
+		String password = "Hello@123";
+		RegisterPage registerPage = new RegisterPage(driver);
+		registerPage.clickSignupLink();
+		registerPage.enterEmailAndPassword(email, password);
+		registerPage.clickLoginButton();
+		ProductPage productPage = new ProductPage(driver);
+		productPage.goToCart();
+		CartPage cartPage = new CartPage(driver);
+		cartPage.goToCheckOut();
+		String checkoutPageTitle = driver.getTitle();
+		Assert.assertTrue(checkoutPageTitle.toLowerCase().contains("checkout"));
+		CheckOutPage checkOutPage = new CheckOutPage(driver);
+		int sum = checkOutPage.getCartSum();
+		int total = checkOutPage.getCartTotal();
+		Assert.assertEquals(sum, total);
+		checkOutPage.orderComment("Test comment for checkout");
+		checkOutPage.placeOrder();
+		String paymentPageTitle = driver.findElement(By.xpath("//li[@class='active']")).getText();
+		Assert.assertTrue(paymentPageTitle.toLowerCase().contains("payment"));
 
-			
-			
-			
-		}
-		
-	
-	
-	
+	}
 
 }
